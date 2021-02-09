b9cd84caccf6d6d2498096c6a04bf325e7e749dc
Арсений <reyziktv@gmail.com>
2021-02-09 07:25:40 +0300

    hometask_1

---
+++ module-1/hometask_1/js/hometask_2.js
@@ -0,0 +1,30 @@
+'use strict';
+let carbon = 89;
+console.log(typeof carbon);
+
+let Diggi = null;
+console.log(typeof null);
+
+let mango = 7;
+console.log(typeof mango);
+
+let car = false;
+console.log(typeof car);
+
+let Bob = true;
+console.log(typeof Bob);
+
+const str = 0;
+console.log(typeof str);
+
+const num = true;
+console.log(typeof num);
+
+const flag = 'JavaScript is awesome!';
+console.log(typeof flag);
+
+const txt = false;
+console.log(typeof txt);
+
+const customer = 3;
+console.log(typeof customer);

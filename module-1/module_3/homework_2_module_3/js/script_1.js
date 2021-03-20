3afe0ca864d2955305ac41fd7a134e067f8fa400
Арсений <reyziktv@gmail.com>
2021-03-20 15:13:45 +0300

    homework_js_module_3

---
+++ module-1/module_3/homework_2_module_3/js/script_1.js
@@ -0,0 +1,30 @@
+"use strict";
+// const user = {
+//     name: 'Mango',
+//     age: 20,
+//     hobby: 'html',
+//     premium: true,
+//   };
+
+//   user.mood= 'happy'
+//   user.hobby= 'skydiving'
+//   user.premium = false
+
+//   for (const key of Object.keys(user)) {
+//       console.log(key+ ': '+ user.key);
+
+//   }
+const user = {
+  name: "Mango",
+  age: 20,
+  hobby: "html",
+  premium: true,
+};
+
+user.mood = "happy";
+user.hobby = "skydiving";
+user["premium"] = false;
+
+for (const key of Object.keys(user)) {
+  console.log(key + ": " + user[key]);
+};

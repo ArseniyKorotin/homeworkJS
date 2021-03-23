a3481595ba87d7ab97d7a594ca9b78e8419a777c
Арсений <reyziktv@gmail.com>
2021-03-23 09:59:51 +0300

    homework_3_module_1_reload_2

--- module-1/hometask_3/js/script_2.js
+++ module-1/hometask_3/js/script_2.js
@@ -1,9 +1,9 @@
 'use strict';
-console.log(Number.parseFloat('5px'));
-const k1 = 5;
-console.log(Number.parseFloat('12djd334'));
-const k2 = 12;
-console.log(Number.parseFloat('12.45asdwe12'));
-const k3 = 12.45;
-console.log(Number.parseFloat('qwqweeewq'));
-const k4 = NaN;
+const k1 = Number.parseFloat('5px');
+console.log(k1);
+const k2 = Number.parseFloat('12djd334');
+console.log(k2);
+const k3 = Number.parseFloat('12.45asdwe12');
+console.log(k3);
+const k4 = Number.parseFloat('qwqweeewq');
+console.log(k4);
